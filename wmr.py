7b80d20bf17c677f765e79c359739e8e4de426ed
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-02-11 20:42:08 +0200

    final commit

--- wmr.py
+++ wmr.py
@@ -7,13 +7,20 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 # Read the image
 image = cv2.imread('w1.png')
 
+# Resize the image to improve processing speed
+resized_image = cv2.resize(image, None, fx=0.5, fy=0.5)
+
 # Convert the image to grayscale
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 
-# Apply thresholding to preprocess the image
-_, threshold_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+# Apply Morphological Operations
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+morph_image = cv2.morphologyEx(gray_image, cv2.MORPH_CLOSE, kernel)
 
-# Use Tesseract to extract text from the image
+# Thresholding
+_, threshold_image = cv2.threshold(morph_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+# Use Tesseract to extract text from the thresholded image
 extracted_text = pytesseract.image_to_string(threshold_image)
 
 # Print the extracted text
